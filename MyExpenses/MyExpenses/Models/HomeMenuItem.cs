67d0f9666d447074ecc054c6ebf11f14e2e9ec3f
Jarosław Pawlak <jaroslaw.pawlak@outlook.com>
2018-12-28 19:25:48 +0100

    import

---
+++ MyExpenses/MyExpenses/Models/HomeMenuItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyExpenses.Models
+{
+    public enum MenuItemType
+    {
+        NewExpense,
+        History,
+        Budgets,        
+        About
+    }
+    public class HomeMenuItem
+    {
+        public MenuItemType Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
